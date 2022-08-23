957ae5850689c2b19e60f1d634254b9f27b1db34
syedm1 <31075630+syedm1@users.noreply.github.com>
2022-08-24 00:16:52 +1000

    calculator unlimited first commit

---
+++ pages/calculator/styles.tsx
@@ -0,0 +1,50 @@
+export const calculatorStyles = {
+  display: "flex",
+  flexFlow: "column",
+  width: "100%",
+  fontFamily: "sans-serif"
+};
+export const displayStyles = {
+  width: "40%",
+  height: "20px",
+  alignSelf: "center",
+  background: "lightgray",
+  padding: "10px",
+  borderRadius: "6px"
+};
+
+export const panelStyles = {
+  display: "flex",
+  justifyContent: "center"
+};
+
+export const numbersStyles = {
+  display: "flex",
+  flexFlow: "wrap",
+  flexWrap: "wrap",
+  justifyItems: "stretch",
+  width: "90px"
+};
+
+export const expressionStyles = {
+  height: "30px",
+  color: "#ed1855"
+};
+
+export const operatorsStyles = {
+  display: "flex",
+  flexFlow: "column"
+};
+
+export const operandsButtonStyles = {
+  color: "white",
+  background: "#ed1855",
+  borderLeft: "none",
+  borderBottom: 0
+};
+
+export const numbersButtonStyles = {
+  fontSize: "1rem",
+  width: "30px",
+  border: "1px solid #ed1855"
+};
